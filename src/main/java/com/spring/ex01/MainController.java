8010dcb30c75c9cec94f30d6a6b653fde2933c2e
Hyeok <ssp02245@gmail.com>
2023-03-09 12:04:30 +0900

    26-2_complete

--- src/main/java/com/spring/ex01/MainController.java
+++ src/main/java/com/spring/ex01/MainController.java
@@ -1,5 +1,38 @@
 package com.spring.ex01;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller("mainController")
+@RequestMapping("/test")
 public class MainController {
+	
+	@RequestMapping(value="/hi.do", method=RequestMethod.GET)
+	public ModelAndView main(HttpServletRequest request,HttpServletResponse response) throws Exception {
+		
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("msg","hihihi");
+		mav.setViewName("main");
+		
+		return mav;
+		
+	}
+	
+	@RequestMapping(value="/hello.do", method= RequestMethod.GET)
+	public ModelAndView main2(HttpServletRequest request,HttpServletResponse response) throws Exception {
+		
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("msg","hello");
+		mav.setViewName("main");
+		
+		return mav;
+		
+	}
+	
 
 }
