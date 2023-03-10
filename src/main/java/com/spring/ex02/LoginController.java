0224bc474dbecec808ea57a3906211fae649cb35
Hyeok <ssp02245@gmail.com>
2023-03-10 11:42:02 +0900

    26.3.5 Model클래스 이용해 값 전달

--- src/main/java/com/spring/ex02/LoginController.java
+++ src/main/java/com/spring/ex02/LoginController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -129,5 +130,26 @@ public class LoginController {
 		
 		return mav;
 	}
+	
+	
+	@RequestMapping(value="/test/login5.do", method= {RequestMethod.GET,RequestMethod.POST})
+	public ModelAndView login5(
+			Model model,
+			HttpServletRequest request, HttpServletResponse response) throws Exception{
+		
+		request.setCharacterEncoding("utf-8");
+		ModelAndView mav = new ModelAndView();
+		
+		
+		System.out.println("isLogin5");
+		model.addAttribute("userID","hong");
+		model.addAttribute("userName","홍길동");
+		model.addAttribute("userEmail","userEmail@email.com");
+		mav.setViewName("result");
+		
+		return mav;
+	}
+	
 }
+
 
